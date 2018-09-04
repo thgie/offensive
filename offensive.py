b94e3d60e59a344e155b3b9fcc2d6fadb3f1775f
Adrian <adrian@thgie.ch>
2018-09-04 23:57:38 +0200

    first commit

---
+++ offensive.py
@@ -0,0 +1,15 @@
+import time
+        
+def offensive():
+     if exists("1536095736317.png") or exists("1536097190007.png"):
+        click("1536096834832.png")
+        click("1536097081756.png")
+        click("1536097108959.png")
+        click("1536097117027.png")
+        click("1536097130835.png")
+
+
+for x in range(10):
+    click("1536095639207.png")
+    type(Key.SPACE)
+    offensive()
